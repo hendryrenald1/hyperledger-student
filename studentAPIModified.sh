fcb042ba5d2c5ea234b669b888848d1d0b9e021d
Hendry <henryreynold@bitbucket.org>
2017-11-30 10:53:22 +0530

    m4

---
+++ studentAPIModified.sh
@@ -0,0 +1,149 @@
+#!/bin/bash
+#
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+jq --version > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+	echo "Please Install 'jq' https://stedolan.github.io/jq/ to execute this script"
+	echo
+	exit 1
+fi
+starttime=$(date +%s)
+
+echo "POST request Enroll on Agent  ..."
+echo
+ORG1_TOKEN=$(curl -s -X POST \
+  http://localhost:4000/users \
+  -H "content-type: application/x-www-form-urlencoded" \
+  -d 'username=Jim&orgName=agent')
+echo $ORG1_TOKEN
+ORG1_TOKEN=$(echo $ORG1_TOKEN | jq ".token" | sed "s/\"//g")
+echo
+echo "ORG1 token is $ORG1_TOKEN"
+echo
+echo "POST request Enroll on Illinois ..."
+echo
+ORG2_TOKEN=$(curl -s -X POST \
+  http://localhost:4000/users \
+  -H "content-type: application/x-www-form-urlencoded" \
+  -d 'username=Barry&orgName=illinois')
+echo $ORG2_TOKEN
+ORG2_TOKEN=$(echo $ORG2_TOKEN | jq ".token" | sed "s/\"//g")
+echo
+echo "ORG2 token is $ORG2_TOKEN"
+echo
+echo
+echo "POST request Create channel  ..."
+echo
+curl -s -X POST \
+  http://localhost:4000/channels \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "content-type: application/json" \
+  -d '{
+	"channelName":"mychannel",
+	"channelConfigPath":"../artifacts/channel/mychannel.tx"
+}'
+echo
+echo
+sleep 5
+echo "POST request Join channel on Agent"
+echo
+curl -s -X POST \
+  http://localhost:4000/channels/mychannel/peers \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "content-type: application/json" \
+  -d '{
+	"peers": ["peer1","peer2"]
+}'
+echo
+echo
+
+echo "POST request Join channel on Illinois"
+echo
+curl -s -X POST \
+  http://localhost:4000/channels/mychannel/peers \
+  -H "authorization: Bearer $ORG2_TOKEN" \
+  -H "content-type: application/json" \
+  -d '{
+	"peers": ["peer1","peer2"]
+}'
+echo
+echo
+
+echo "POST Install chaincode on Agent"
+echo
+curl -s -X POST \
+  http://localhost:4000/chaincodes \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "content-type: application/json" \
+  -d '{
+	"peers": ["peer1", "peer2"],
+	"chaincodeName":"studentcc",
+	"chaincodePath":"github.com/student_cc",
+	"chaincodeVersion":"v0"
+}'
+echo
+echo
+
+
+echo "POST Install chaincode on Illinios"
+echo
+curl -s -X POST \
+  http://localhost:4000/chaincodes \
+  -H "authorization: Bearer $ORG2_TOKEN" \
+  -H "content-type: application/json" \
+  -d '{
+	"peers": ["peer1","peer2"],
+	"chaincodeName":"studentcc",
+	"chaincodePath":"github.com/student_cc",
+	"chaincodeVersion":"v0"
+}'
+echo
+echo
+
+read
+
+echo "POST instantiate chaincode on peer1 of Agent"
+echo
+curl -s -X POST \
+  http://localhost:4000/channels/mychannel/chaincodes \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "content-type: application/json" \
+  -d '{
+	"chaincodeName":"studentcc",
+	"chaincodeVersion":"v0",
+	"args":["a","{\"Name\":\"Testing\"}"]
+}'
+
+
+echo
+echo "Press any Key to Add  the chain code"
+read 
+
+echo "POST invoke chaincode on both the peers"
+echo
+TRX_ID=$(curl -s -X POST \
+  http://localhost:4000/channels/mychannel/chaincodes/studentcc \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "content-type: application/json" \
+  -d '{
+	"fcn":"add",
+	"args":["a", "{\"UName\":\"First University\"}"]
+}')
+echo "Transacton ID is $TRX_ID"
+echo
+echo
+
+echo "GET query chaincode on peer1 of Org1"
+echo
+curl -s -X GET \
+  "http://localhost:4000/channels/mychannel/chaincodes/studentcc?peer=peer1&fcn=get&args=%5B%22a%22%5D" \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "content-type: application/json"
+echo
+echo
+
+echo "Total execution time : $(($(date +%s)-starttime)) secs ..."
